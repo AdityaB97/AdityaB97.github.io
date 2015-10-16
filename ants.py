b6950fed5959ba4b3eb81f9ea9dee58bbfb185e2
rohan balan <rbalan@berkeley.edu>
2015-10-15 21:13:20 -0700

    commit

--- ants.py
+++ ants.py
@@ -1,4 +1,5 @@
 """The ants module implements game logic for Ants Vs. SomeBees."""
+#test comment
 
 import random
 import sys
@@ -905,4 +906,3 @@ def run(*args):
     Insect.reduce_armor = class_method_wrapper(Insect.reduce_armor,
             pre=print_expired_insects)
     start_with_strategy(args, interactive_strategy)
-
